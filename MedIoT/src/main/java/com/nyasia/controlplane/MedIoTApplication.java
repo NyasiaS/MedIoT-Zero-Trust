cbd0a99087f62f3ed7a92788d2707258a212f38f
Nyasia Spearman <97766350+NyasiaS@users.noreply.github.com>
2026-03-31 10:37:08 -0400

    Add files via upload

---
+++ MedIoT/src/main/java/com/nyasia/controlplane/MedIoTApplication.java
@@ -0,0 +1,13 @@
+package com.nyasia.controlplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedIoTApplication.class, args);
+	}
+
+}
